05b494daaeb67c6556d3b9b55df8197eef5f6399
Sofia Katiran <sofiakatiran@gmail.com>
2023-09-13 17:43:07 +0330

    feat (coffee-store page) : creat coffee store html elements

--- coffee-stores/pages/coffee-store/[id].jsx
+++ coffee-stores/pages/coffee-store/[id].jsx
@@ -1,51 +1,86 @@
 import { useRouter } from "next/router";
 import Link from "next/link";
-import Head from 'next/head'
-import coffeeStoresData from '../../data/coffee-stores.json'; 
+import Head from "next/head";
+import Image from "next/image";
+import coffeeStoresData from "../../data/coffee-stores.json";
 
-export function getStaticProps(staticProps){
+export function getStaticProps(staticProps) {
   console.log(staticProps);
-  const params = staticProps.params
-  return{
+  const params = staticProps.params;
+  return {
     props: {
-      coffeeStore: coffeeStoresData.find(coffeeStore => {
-        return coffeeStore.id.toString() === params.id//dynamic id
-      })
-    }
-  }
+      coffeeStore: coffeeStoresData.find((coffeeStore) => {
+        return coffeeStore.id.toString() === params.id; //dynamic id
+      }),
+    },
+  };
 }
 
-export function getStaticPaths(){
+export function getStaticPaths() {
   const paths = coffeeStoresData.map((coffeeStore) => {
-    return{
-      params:{
-        id: coffeeStore.id.toString()
-      }
-    }
-  })
-  return{
-   paths,
-    fallback:true,
-  }
+    return {
+      params: {
+        id: coffeeStore.id.toString(),
+      },
+    };
+  });
+  return {
+    paths,
+    fallback: true,
+  };
 }
 
 function coffeeStore(props) {
   const router = useRouter();
-  console.log(router);
 
-  if(router.isFallback){
-    return <div>Loadding ...</div>
+  if (router.isFallback) {
+    return <div>Loadding ...</div>;
   }
-  const { address, name, neighbourhood} = props.coffeeStore 
+
+  const { address, name, neighbourhood, imgUrl } = props.coffeeStore;
+
+ const handleUpvoteButton =()=>{
+  console.log('handle up vote');
+ }
+
   return (
-    <div>
+    <div className="layout">
       <Head>
         <title>{name}</title>
       </Head>
-      <Link href="/">Back to home</Link>
-      <p>{address}</p>
-      <p>{name}</p>
-      <p>{neighbourhood}</p>
+      <div className="container">
+        <div className="col1">
+          <div className="backtohomeLink">
+            <Link href="/">Back to home</Link>
+          </div>
+          <div className="nameWrapper">
+          <h1 className="name ">{name}</h1>
+          </div>
+          <Image
+            src={imgUrl}
+            width={600}
+            height={360}
+            className="storeImg"
+            alt={name}
+          ></Image>
+        </div>
+        <div className="col2 glass">
+          <div className="iconWrapper">
+            <Image src='/static/icons/places.svg' width='24' height='24'/>
+          <p className="text">{address}</p>
+          </div>
+          <div className="iconWrapper">
+            <Image src='/static/icons/nearMe.svg' width='24' height='24'/>
+          <p className="text">{neighbourhood}</p>
+          </div>
+          <div className="iconWrapper">
+            <Image src='/static/icons/star.svg' width='24' height='24'/>
+          <p className="text">1</p>
+          </div>
+
+          <button className="upvoteBtn" onClick={handleUpvoteButton}> Up vote</button>
+        </div>
+      </div>
     </div>
   );
 }
